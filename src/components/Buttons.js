f30fe64cb14ce3b55783520fe5b9ac90a9f674fe
Joel Foust <joel.foust@yahoo.com>
2023-08-11 17:36:33 -0400

    Assignment 6 in progess

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,22 @@
-const Buttons = () => {
+import './styling.css';
+import satData from './satData';
+
+const Buttons = (props) => {
+
+  
+  
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container">
+      {props.displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => props.filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={props.setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
